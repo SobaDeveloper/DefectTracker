6a904a1d4f561742eb046e96fffe87f1da8bf1d6
SobaDeveloper <levi.hsiao@gmail.com>
2014-08-25 17:31:22 -0700

    Test from laptop

--- Team B Defect Tracker/src/edu/uci/java2/Main.java
+++ Team B Defect Tracker/src/edu/uci/java2/Main.java
@@ -1,5 +1,14 @@
 package edu.uci.java2;
 
+/**
+ * X460.11/1 - Java Programming II - Team B
+ * Main.java
+ * Purpose: Main executable
+ * 
+ * @author Shaun Adriano, Dennis Hom, Levi Hsiao, Susan Marosek
+ * @version 1.0 8/25/2014
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
